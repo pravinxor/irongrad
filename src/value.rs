d148510e76c953151da11de869649aea8d9f113b
Pravin Ramana <pravinramana25@protonmail.ch>
2023-05-10 20:40:35 -0500

    Keep track of the operations that led up to value with a simple DAG

--- src/value.rs
+++ src/value.rs
@@ -1,22 +1,73 @@
+use std::rc::Rc;
+
 #[derive(Debug)]
-pub struct Value<T> {
-    pub data: T,
+pub enum Operation {
+    Add,
+    Mul,
 }
 
-impl<T: std::ops::Add<Output = T>> std::ops::Add<Value<T>> for Value<T> {
-    type Output = Value<T>;
-    fn add(self, rhs: Value<T>) -> Self::Output {
-        Self::Output {
-            data: self.data + rhs.data,
+#[derive(Debug)]
+/// Struct holding the lhs and rhs values that were combined with an operation
+pub struct Previous<T> {
+    lhs: Rc<InnerValue<T>>,
+    rhs: Rc<InnerValue<T>>,
+}
+
+#[derive(Debug)]
+pub struct InnerValue<T> {
+    /// The value stored
+    pub data: T,
+    /// The operation that created the value (None if the value was initialized with Self::new())
+    op: Option<Operation>,
+    // The previous operations that created the value (Also None if the value was initialized with Self::new())
+    prev: Option<Previous<T>>,
+}
+
+#[derive(Debug)]
+pub struct Value<T> {
+    pub inner: Rc<InnerValue<T>>,
+}
+
+impl<T> Value<T> {
+    pub fn new(data: T) -> Self {
+        Self {
+            inner: Rc::new(InnerValue {
+                data,
+                op: None,
+                prev: None,
+            }),
         }
     }
 }
 
-impl<T: std::ops::Mul<Output = T>> std::ops::Mul<Value<T>> for Value<T> {
+impl<T: std::ops::Add<Output = T> + Copy> std::ops::Add<Value<T>> for Value<T> {
+    type Output = Value<T>;
+    fn add(self, rhs: Value<T>) -> Self::Output {
+        Self::Output {
+            inner: Rc::new(InnerValue {
+                data: self.inner.data + rhs.inner.data,
+                op: Some(Operation::Add),
+                prev: Some(Previous {
+                    lhs: self.inner,
+                    rhs: rhs.inner,
+                }),
+            }),
+        }
+    }
+}
+
+impl<T: std::ops::Mul<Output = T> + Copy> std::ops::Mul<Value<T>> for Value<T> {
     type Output = Value<T>;
     fn mul(self, rhs: Value<T>) -> Self::Output {
         Self::Output {
-            data: self.data * rhs.data,
+            inner: Rc::new(InnerValue {
+                data: self.inner.data * rhs.inner.data,
+                op: Some(Operation::Mul),
+                prev: Some(Previous {
+                    lhs: self.inner,
+                    rhs: rhs.inner,
+                }),
+            }),
         }
     }
 }
