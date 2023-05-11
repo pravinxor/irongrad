c4ae87900f9394cfdb875164b6cf5cb94e5b12c7
Pravin Ramana <pravinramana25@protonmail.ch>
2023-05-11 10:56:19 -0500

    Implement topological sorting

--- src/value.rs
+++ src/value.rs
@@ -1,5 +1,3 @@
-use std::rc::Rc;
-
 #[derive(Debug)]
 pub enum Operation {
     Add,
@@ -10,8 +8,8 @@ pub enum Operation {
 #[derive(Debug)]
 /// Struct holding the lhs and rhs values that were combined with an operation
 pub struct Previous {
-    lhs: Option<Rc<InnerValue>>,
-    rhs: Option<Rc<InnerValue>>,
+    lhs: Option<std::rc::Rc<InnerValue>>,
+    rhs: Option<std::rc::Rc<InnerValue>>,
 }
 
 #[derive(Debug)]
@@ -25,26 +23,53 @@ pub struct InnerValue {
     /// The operation that created the value (None if the value was initialized with Self::new())
     op: Option<Operation>,
 
-    /// The previous operations that created the value (Also None if the value was initialized with Self::new())
-    prev: Previous,
+    /// The previous value(s) that created the current value
+    previous: Vec<std::rc::Rc<InnerValue>>,
+}
+
+impl PartialEq for InnerValue {
+    fn eq(&self, other: &Self) -> bool {
+        std::ptr::eq(self, other)
+    }
+}
+
+impl Eq for InnerValue {}
+
+impl std::hash::Hash for InnerValue {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        std::ptr::hash(self, state);
+    }
+}
+
+impl InnerValue {
+    pub fn build_topo<'a: 'b, 'b>(
+        &'a self,
+        topo: &mut Vec<&'a InnerValue>,
+        visited: &mut std::collections::HashSet<&'b InnerValue>,
+    ) {
+        if !visited.contains(&self) {
+            visited.insert(self);
+            self.previous
+                .iter()
+                .for_each(|child| child.build_topo(topo, visited));
+            topo.push(self);
+        }
+    }
 }
 
 #[derive(Debug)]
 pub struct Value {
-    pub inner: Rc<InnerValue>,
+    pub inner: std::rc::Rc<InnerValue>,
 }
 
 impl Value {
     pub fn new(data: f64) -> Self {
         Self {
-            inner: Rc::new(InnerValue {
+            inner: std::rc::Rc::new(InnerValue {
                 data,
                 grad: 0.0, // Initialize to default/0 signifying no effect on gradient
                 op: None,
-                prev: Previous {
-                    lhs: None,
-                    rhs: None,
-                },
+                previous: vec![],
             }),
         }
     }
@@ -53,16 +78,20 @@ impl Value {
         let x = self.inner.data;
         let t = ((2.0 * x).exp() - 1.0) / ((2.0 * x).exp() + 1.0); // The actual tanh computation
         Self {
-            inner: Rc::new(InnerValue {
+            inner: std::rc::Rc::new(InnerValue {
                 data: t,
                 grad: 0.0,
                 op: Some(Operation::Tanh),
-                prev: Previous {
-                    lhs: Some(self.inner),
-                    rhs: None,
-                },
+                previous: vec![self.inner],
             }),
         }
+    }
+
+    pub fn build_topo(&self) -> Vec<&InnerValue> {
+        let mut topo = Vec::new();
+        let mut visited = std::collections::HashSet::new();
+        self.inner.build_topo(&mut topo, &mut visited);
+        topo
     }
 }
 
@@ -70,14 +99,11 @@ impl std::ops::Add<Value> for Value {
     type Output = Value;
     fn add(self, rhs: Value) -> Self::Output {
         Self::Output {
-            inner: Rc::new(InnerValue {
+            inner: std::rc::Rc::new(InnerValue {
                 data: self.inner.data + rhs.inner.data,
                 grad: 0.0,
                 op: Some(Operation::Add),
-                prev: Previous {
-                    lhs: Some(self.inner),
-                    rhs: Some(rhs.inner),
-                },
+                previous: vec![self.inner, rhs.inner],
             }),
         }
     }
@@ -87,14 +113,11 @@ impl std::ops::Mul<Value> for Value {
     type Output = Value;
     fn mul(self, rhs: Value) -> Self::Output {
         Self::Output {
-            inner: Rc::new(InnerValue {
+            inner: std::rc::Rc::new(InnerValue {
                 data: self.inner.data * rhs.inner.data,
                 grad: 0.0,
                 op: Some(Operation::Mul),
-                prev: Previous {
-                    lhs: Some(self.inner),
-                    rhs: Some(rhs.inner),
-                },
+                previous: vec![self.inner, rhs.inner],
             }),
         }
     }
